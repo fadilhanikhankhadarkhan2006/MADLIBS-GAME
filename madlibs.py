81a31485568e9f02e91cded342ddf024d60e6cc7
Fadil Hani Khan K <fadilhanikhankhadarkhan2006@gmail.com>
2026-06-01 11:11:46 +0530

    Add madlibs.py for interactive story generation

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter another adjective: ")
+adjective3 = input("Enter one more adjective: ")
+animal1 = input("Enter an animal: ")    
+verb1 = input("Enter a verb: ")
+noun1 = input("Enter a noun: ")
+
+
+
+print(f"Today I went to a {adjective1} zoo")
+print(f"I saw a {adjective2} {animal1} jumping up and down in its tree")
+print(f"He {verb1} through the large tunnel that led to its {adjective3} {noun1}")
